f6cdd5797b2398edaacf8bc4ad35be6e8a97c73d
veehra <mmesoma.vera@gmail.com>
2022-10-14 14:34:12 -0700

    three

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,4 +6,32 @@
  */
 int main(void)
 {
+	int i;
+	int j;
+	int k;
 
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (i != j != k && i < j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i == '7' && j == '8' && k == '9')
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
