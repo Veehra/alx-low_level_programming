807d4c79a185649ba93b442b755493f2eed0ce12
veehra <mmesoma.vera@gmail.com>
2022-10-14 13:41:01 -0700

    digit:wq

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,22 +8,18 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = '0'; i < '8'; i++)
+	for (i = '0'; i < '9'; i++)
 	{
-		for (j = i + 1; j < '9'; j++)
+		for (j = i + 1; j < 10; j++)
 		{
-			for (k = j + 1; j < 10; j++)
+			putchar(i);
+			putchar(j);
+			if (i == 7 && j == 8 && k == 9)
 			{
-				putchar(i);
-				putchar(j);
-				putchar(k);
-				if (i == 7 && j == 8 && k == 9)
-				{
-					continue;
-				}
-				putchar(',');
-				putchar(' ');
+				continue;
 			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
