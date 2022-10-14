238ecd49d64493499b2be782ee037ceffa8cdfc4
veehra <mmesoma.vera@gmail.com>
2022-10-14 13:37:26 -0700

    lol

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i < '8'; i++)
+	{
+		for (j = i + 1; j < '9'; j++)
+		{
+			for (k = j + 1; j < 10; j++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == 7 && j == 8 && k == 9)
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
