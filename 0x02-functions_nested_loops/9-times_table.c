ea80b22adfa511a4393141882443e832b33d2ef0
veehra <mmesoma.vera@gmail.com>
2022-10-18 12:57:25 -0700

    times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * times_table - for a time table
+ *
+ * Return: Always 0
+ */
+void times_table(void)
+{
+	int i, j, k, first, last;
+
+	for (i = 0; i <= 9; j++)
+	{
+		k = j * i;
+
+		if (j > 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			if (k < 10)
+			{
+				_putchar(' ');
+			}
+		}
+
+		if (k > 0)
+		{
+			first = k;
+
+			while (first >= 10)
+				first = first / 10;
+
+			last = k % 10;
+
+			_putchar(first + 48);
+			_putchar(last + 48);
+		}
+		else
+		{
+			_putchar(k + 48);
+		}
+	}
+	_putchar('\n');
+}
