992abc8a3ccaf89c31cf50dadd7f11df890936a2
veehra <mmesoma.vera@gmail.com>
2022-10-20 11:13:01 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,23 +11,22 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Fizz");
+			printf("FizzBuzz ");
 		}
 		else if  (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if (i % 3 == 0)
 		{
-			printf("FizzBuzz");
+			printf("Fizz ");
 		}
 		else
 		{
-			printf("%d\n", i);
+			printf("%d", i);
 		}
+		printf("\n");
 	}
-
-	return (0);
 }
