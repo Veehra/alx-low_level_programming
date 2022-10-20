b10d6b4e169aea08dff85a1c5b9045f2949b1345
veehra <mmesoma.vera@gmail.com>
2022-10-20 08:43:48 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - to pick upperr case letter
+ * @c: for single character
+ *
+ * Return: 1if true, 0 if false
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
