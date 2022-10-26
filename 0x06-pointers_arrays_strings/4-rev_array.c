4f4d42c1782595e5d358432019337eb5c7695b78
Veehra <mmesoma.vera@gmail.com>
2022-10-26 01:53:36 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - Entry point
+ * @a: array pointer
+ * @n: integer
+ *
+ * Return: void;
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, v;
+
+	for (i = 0; i < n; i++)
+	{
+		v = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = v;
+	}
+}
