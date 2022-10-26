2fedd5ce3622ab0e72b8dbbb366d2c443cdd8050
Veehra <mmesoma.vera@gmail.com>
2022-10-26 01:08:01 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - Entry point
+ * @dest: First pointer
+ * @src: Second pointer
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+}
