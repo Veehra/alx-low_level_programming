c8fd26a4a0627661db6d8cdd2e30d304c216f3cb
Veehra <mmesoma.vera@gmail.com>
2022-10-27 06:30:32 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,20 +11,23 @@
 char *leet(char *str)
 {
 	int i, j, len;
-	char capit[5] = {'A', 'E', 'O', 'T', 'L'};
-	char small[5] = {'a', 'e', 'o', 't', 'l'};
-	char num[5] = {'4', '3', '0', '7', '1'};
+	char capit[6] = {'A', 'E', 'O', 'T', 'L'};
+	char small[6] = {'a', 'e', 'o', 't', 'l'};
+	char num[6] = {'4', '3', '0', '7', '1'};
 
 	len = strlen(str);
 
 	for (i = 0; i <= 5; i++)
 	{
-		for (j = 0; j <= 5; j++)
-			if (str[i] == capit[j] && str[i] == small[j])
+		for (j = 0; j < 5; j++)
+		{
+			if (str[i] == capit[j] || str[i] == small[j])
 			{
 				str[i] = num[j];
+				break;
 			}
-	}
+		}
 
-	return (str);
+		return (str);
+	}
 }
