9463570a09106796b2e3fd0da5d72eef32050eff
Veehra <mmesoma.vera@gmail.com>
2022-10-27 05:37:55 -0700

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * cap_string - Entry point
+ * @str: pointer
+ *
+ * Return: nothing
+ */
+
+char *cap_string(char *str)
+{
+	int i, len;
+	char k = ' ';
+	char all[13] = " ,;.!?\"(){}";
+
+	len = strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		else if (str[1 - 1] == all[13])
+		{
+			str[i] = str[i] - 32;
+		}
+		else
+		{
+			k = str[i];
+		}
+	}
+
+	return (str);
+}
