893cd9f3bb755254998d4e6e734720ed41565e84
Veehra <mmesoma.vera@gmail.com>
2022-10-27 06:11:46 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * leet - Entry point
+ *
+ * @str: pointer
+ *
+ * Return: nothing
+ */
+char *leet(char *str)
+{
+	int i, j, len;
+	char capit[5] = {'A', 'E', 'O', 'T', 'L'};
+	char small[5] = {'a', 'e', 'o', 't', 'l'};
+	char num[5] = {'4', '3', '0', '7', '1'};
+
+	len = strlen(str);
+
+	for (i = 0; i <= 5; i++)
+	{
+		if (str[i] == capit[j] && str[i] == small[j])
+		{
+			str[i] = num[j];
+		}
+	}
+
+	return (str);
+}
