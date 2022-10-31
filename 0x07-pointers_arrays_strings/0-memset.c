eefa7fb8d2845b5926e1e5f8865b36f2dbaf194a
Veehra <mmesoma.vera@gmail.com>
2022-10-31 06:49:05 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _memset - Entry point
+ * @s: pointer
+ * @b: pointer
+ * @n: pointer
+ *
+ * Return: void
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+}
