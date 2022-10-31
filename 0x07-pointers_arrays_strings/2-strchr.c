c7f27973862dce037acb4b00e99666a8d709a217
Veehra <mmesoma.vera@gmail.com>
2022-10-31 07:22:36 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strchr - Entry point
+ * @s: pointer
+ * @c: integer
+ *
+ * Return: void
+ */
+
+char *_strchr(char *s, char c)
+{
+	strchr(s, c);
+
+	return (s);
+}
