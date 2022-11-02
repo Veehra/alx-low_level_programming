e1abab564173d8abfabe59fd0e03f27187364b7a
Veehra <mmesoma.vera@gmail.com>
2022-11-02 08:09:26 -0700

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - Entry point
+ * @s: pointer
+ *
+ * Return: void
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = strlen(s);
+
+	return (i);
+}
