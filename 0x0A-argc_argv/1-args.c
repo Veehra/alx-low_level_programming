96e80b6e74587a52ab77af9e8e4b81a336410eaa
Veehra <mmesoma.vera@gmail.com>
2022-11-04 08:15:05 -0700

    argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,14 +3,14 @@
 #include <stdlib.h>
 
 /**
- * arg - Entry point
+ * main - Entry point
  * @argc: prints the number of argument
  * @argv: prints arg, in an array
  *
  * Return: 0;
  */
 
-int arg(int argc, char __attribute__((unused)) *argv)
+int main(int argc, char __attribute__((unused)) *argv[])
 {
 	printf("%d\n", argc - 1);
 
