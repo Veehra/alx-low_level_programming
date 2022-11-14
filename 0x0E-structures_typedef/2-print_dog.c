ce362cd9bd6032ebfc88908db67494d62f2d9c03
Veehra <mmesoma.vera@gmail.com>
2022-11-14 11:27:23 -0800

    print

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+
+/**
+ * print_dog - Entry point
+ *
+ * @d: pointer
+ *
+ * Return: a pointer
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+
+		printf("Name: %f\n", d->age);
+
+		if (d->owner)
+			printf("Owner.name: %s\n", d->owner);
+		else
+			printf("Name: (nil)\n");
+	}
+}
